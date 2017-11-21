91b7b0fa5a1135fe087472daa127b8fbb632c3ce
Romain Pouclet <romain.pouclet@gmail.com>
2017-11-21 15:20:04 -0500

    Initial Commit

---
+++ RandomUserKit/RandomUserKit.h
@@ -0,0 +1,19 @@
+//
+//  RandomUserKit.h
+//  RandomUserKit
+//
+//  Created by Romain Pouclet on 2017-11-21.
+//  Copyright © 2017 Buddybuild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomUserKit.
+FOUNDATION_EXPORT double RandomUserKitVersionNumber;
+
+//! Project version string for RandomUserKit.
+FOUNDATION_EXPORT const unsigned char RandomUserKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomUserKit/PublicHeader.h>
+
+
